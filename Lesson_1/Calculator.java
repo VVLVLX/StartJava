15cd3cf538702ea5446e29f5dbba5b8565ab78cb
VVLVLX <vvlvlxndr@yandex.ru>
2023-05-10 22:51:52 +0300

    removed comments and duplicate code

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,39 +1,37 @@
 public class Calculator {
 
     public static void main(String[] args) {
-    //Калькулятор
         System.out.println("Калькулятор");
-        int a = 10; // Введи число;
-        int b = 3; // Введи число;
-        char sign = '?'; // Введи знак арифметической операции в '': +, -, *, /, ^, %;
-        int result = a;
+        int a = 10;
+        int b = 3;
+        char sign = '^';
+        int result = 0;
         if (a % 1 == 0 && a > 0 && b % 1 == 0 && b > 0) {
-            if (sign == '+') {
-                result = a + b;
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else if (sign == '-') {
-                result = a - b;
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else if (sign == '*') {
-                result = a * b;
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else if (sign == '/') {
-                result = a / b;
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else if (sign == '^') {
-                for (int i = 1; i < b; i++) {
-                    result *= a;
+            if (sign == '+' || sign == '-' || sign == '*' ||
+                    sign == '/' || sign == '^' || sign == '%') {
+                if (sign == '+') {
+                    result = a + b;
+                } else if (sign == '-') {
+                    result = a - b;
+                } else if (sign == '*') {
+                    result = a * b;
+                } else if (sign == '/') {
+                    result = a / b;
+                } else if (sign == '^') {
+                    for (int i = 1; i < b; i++) {
+                        result *= a;
+                    }
+                } else if (sign == '%') {
+                    result = a % b;
                 }
                 System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else if (sign == '%') {
-                result = a % b;
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
             } else {
-                System.out.println("Ошибка! Можно вводить только знаки" + 
-                        " арифметических операций: +, -, *, /, ^, %");
+                System.out.println("Ошибка! Можно вводить только знаки" +
+                    " арифметических операций: +, -, *, /, ^, %");
             }
         } else {
             System.out.println("Ошибка! Можно вводить только целые, положительные числа!");
         }
     }
 }
+
