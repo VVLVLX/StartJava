f3e10b90eb995e3676901f1ac9de03968c36e736
VVLVLX <vvlvlxndr@yandex.ru>
2023-05-10 22:53:39 +0300

    removed comments, typos and duplicate code

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,36 +1,26 @@
 public class MyFirstGame {
 
     public static void main(String[] args) {
-        //Игра Угадай число
         System.out.println("Игра Угадай число");
         int leftExcluding = 0;
         int rightIncludig = 100;
-        int hiddenNum = 43; //Компьютер загадывает целое число в полуинтервале (0, 100];
-        int playerNum = 37; //Число, которое предлагает игрок;
-        if (playerNum > leftExcluding && playerNum <= rightIncludig){
+        int hiddenNum = 30;
+        int playerNum = 20;
+        if (playerNum > leftExcluding && playerNum <= rightIncludig) {
             if (playerNum > hiddenNum) {
                 do {
-                    if (playerNum == hiddenNum) {
-                        System.out.println("Вы победили!");
-                    } else {
-                        System.out.println("Число " + playerNum + " больше того, что загадал компуктер!");
-                    }
+                    System.out.println("Число " + playerNum + " больше того, что загадал компьютер!");
                     playerNum--;
-                } while (playerNum >= hiddenNum);
+                } while (playerNum > hiddenNum);
             } else if (playerNum < hiddenNum) {
                 do {
-                    if (playerNum == hiddenNum) {
-                        System.out.println("Вы победили!");
-                    } else {
-                        System.out.println("Число " + playerNum + " меньше того, что загадал компуктер!");
-                    }
+                    System.out.println("Число " + playerNum + " меньше того, что загадал компьютер!");
                     playerNum++;
-                } while (playerNum <= hiddenNum);
-            } else {
-                System.out.println("Вы победили!");
+                } while (playerNum < hiddenNum);
             }
+            System.out.println("Вы победили!");
         } else {
-            System.out.println("Компуктер загадывает целые числа в полуинтервале (0, 100]!");
+            System.out.println("Компьютер загадывает целые числа в полуинтервале (0, 100]!");
         }
     }
 }
