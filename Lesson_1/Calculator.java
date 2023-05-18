e1f931b97b01f6fac2b7012bdbe3146393c5d4ec
VVLVLX <vvlvlxndr@yandex.ru>
2023-05-19 00:09:52 +0300

    changed the branching to a case, added a division-by-zero check.

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,36 +2,43 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("Калькулятор");
-        int a = 10;
-        int b = 3;
-        char sign = '^';
-        int result = 0;
-        if (a % 1 == 0 && a > 0 && b % 1 == 0 && b > 0) {
-            if (sign == '+' || sign == '-' || sign == '*' ||
-                    sign == '/' || sign == '^' || sign == '%') {
-                if (sign == '+') {
+        int a = 12;
+        int b = 0;
+        char sign = '/';
+        if (a % 1 == 0 && a >= 0 && b % 1 == 0 && b >= 0) {
+            int result = 1;
+            switch(sign) {
+                case '+':
                     result = a + b;
-                } else if (sign == '-') {
+                    break;
+                case '-':
                     result = a - b;
-                } else if (sign == '*') {
+                    break;
+                case '*':
                     result = a * b;
-                } else if (sign == '/') {
-                    result = a / b;
-                } else if (sign == '^') {
-                    for (int i = 1; i < b; i++) {
+                    break;    
+                case '/':
+                    if (b == 0) {
+                        System.out.println("Ошибка! На ноль делить нельзя!");
+                    } else {
+                        result = a / b;
+                    }
+                    break;
+                case '^':
+                    for (int i = 1; i <= b; i++) {
                         result *= a;
                     }
-                } else if (sign == '%') {
-                    result = a % b;
-                }
-                System.out.println(a + "" + sign + "" + b + "=" + "" + result);
-            } else {
-                System.out.println("Ошибка! Можно вводить только знаки" +
-                    " арифметических операций: +, -, *, /, ^, %");
+                    break;
+                case '%':
+                    result = a + b;
+                    break;
+                default:
+                    System.out.println("Ошибка! Можно вводить только знаки" +
+                            " арифметических операций: +, -, *, /, ^, %");
             }
+            System.out.println(a + "" + sign + "" + b + "=" + "" + result);
         } else {
             System.out.println("Ошибка! Можно вводить только целые, положительные числа!");
         }
     }
 }
-
